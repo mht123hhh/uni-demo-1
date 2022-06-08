990d76f8fc9ce5e5264b400d4b11ae813ede1bb2
章岳腾 <1669126471@qq.com>
2022-06-08 11:20:10 +0800

    完成了tabbar的功能

---
+++ pages/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    <text>home</text>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
